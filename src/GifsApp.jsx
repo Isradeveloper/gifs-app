280f4a31df2d1678dcd6a29cc748d4a1d1e1a7e2
Israel Trujillo Domínguez <ingisraeltrujillo@gmail.com>
2022-09-14 09:48:27 -0500

    Creación de Componentes App e Input

---
+++ src/GifsApp.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react'
+import { AddCategory } from './components/AddCategory'
+
+export const GifsApp = () => {
+
+  //Cuando queremos guardar información y esa información cambia el HTML usamos hook useState
+  const [ categories, setCategories ] = useState([ 'One piece', 'Cristiano Ronaldo' ])
+
+  const onAddCategory = () => {
+    setCategories(['Valorant', ...categories])
+  }
+
+  return (
+    <>
+      {/* Titulo */}
+      <h1>GifsApp</h1>
+
+      {/* Input */}
+      <AddCategory setCategories = {setCategories} categories={categories}/>
+
+      {/* Listado de Gifs */}
+      <ol>
+        { categories.map(category => { //Generamos los li con .map
+            return <li key={category}>{category}</li> //Cuando generamos elementos dinamicos debemos usar keys
+        }) }
+      </ol>
+      
+        {/* Gif item */}
+    </>
+  )
+}
---
+++ src/components/AddCategory.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import { useState } from 'react'
+
+
+export const AddCategory = ({setCategories}) => {
+
+  const [inputValue, setInputValue] = useState('') // Utilizamos useState para tener la informacion del input en el html
+
+  
+  const onInputChange = ({ target }) => { //Desestructuramos el target de e
+    // console.log(target.value)
+    setInputValue(target.value) // Asignamos lo que está escrito en el hook
+  }
+
+
+  const onSubmit = (event)=>{
+    event.preventDefault() // Evitamos que se reinicie la pagina
+    if (inputValue.trim().length <=1) return // Solo se ejecuta si tenemos mas de 1 letra
+    setCategories((categories)=>[inputValue, ...categories]) //El set tiene un callback del dato anterior
+    setInputValue("")
+  }
+
+  return (
+    <>
+      <form onSubmit={(event)=> onSubmit(event) /*Utilizamos el evento onSubmit*/}>  
+        <input type="text"
+          name="buscar-gif"
+          id="buscar-gif"
+          placeholder='Buscar gifs'
+          value={inputValue} // Se le asigna el valor el hook al input
+          onChange={(event) => onInputChange(event)} // Utilizamos la función onInputChange
+        />
+      </form>
+    </>
+  )
+}
