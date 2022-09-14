d8e8bc54d2fb2cf137b354b307645f2938139f9c
Israel Trujillo Domínguez <ingisraeltrujillo@gmail.com>
2022-09-14 11:16:44 -0500

    Arreglo para que solo se muestre una vez el fetch

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,8 +1,13 @@
+import { useEffect } from 'react'
 import {getGifs} from '../helpers/getGifs'
+
 
 export const GifGrid = ({ category }) => {
 
-  getGifs(category)
+  useEffect( ()=>{  //Dispara un efecto cuando el componente se renderiza por primera vez
+    getGifs(category)
+  }, [] )
+
   return (
     <>
       <h3>{category}</h3>
--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import { GifsApp } from './GifsApp'
 import './styles.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  // <React.StrictMode>
     <GifsApp/>
-  </React.StrictMode>
+  //</React.StrictMode>
 )
